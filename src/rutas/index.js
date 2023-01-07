359a73b6b675de7508039a3e9fc748a4dd3c1b8a
Raquel laPaz <lunablankaa25@hotmail.es>
2023-01-07 13:42:08 +0100

    node-instagram

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -1,7 +1,12 @@
+const { Router } = require('express');
 const express= require('express');
 const rutas = express.Router();
 
+const Instagram = require('node-instagram');
+
 rutas.get('/', (req, res) => {
-    res.send('Index');
-})
+    res.render('Index');
+});
+
+Router.get()
 module.exports = rutas;// exportando rutas.
