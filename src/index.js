28dce80f1371bab4cecb6c1cc24f149614d7a5a8
Raquel laPaz <lunablankaa25@hotmail.es>
2023-01-09 23:17:20 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const uuid = require('uuidv4');
+
+
+const morgan = require('morgan');
+
+
+//incializacion
+const app = express();
+require('./database');
+
+//Configuraciones
+
+app.set('port', process.env.PORT || 3000); 
+app.set('views', path.join(__dirname, 'views'));//unir carpeta vistas
+app.set('view engine', 'ejs');
+
+
+//middlewares(primero pasa por aqui antes de pasar x las rutas)
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));//Para los datos de los formularios
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/img/uploads'),
+    filename: (req, file, cb, filename) => {
+        cb(null, uuid + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage : storage }).single('image'));
+
+//Variables Globales
+
+
+//Rutas
+app.use(require('./rutas/index')); 
+
+//Archivos estaticos (donde van las imagenes subidas)
+
+
+//Start servidor
+app.listen(app.get('port'), ()=> {
+    console.log(`Servidor en el puerto ${app.get('port')}`);
+});
---
+++ src/rutas/index.js
@@ -0,0 +1,47 @@
+const { Router} = require('express');
+const ruta = Router();
+
+const Image = require('../archivos/image');
+
+ruta.get('/', (req, res) => {
+   res.send('Index');
+});
+
+//ruta para mostrar el formulario
+ruta.get('/upload', (req, res) =>{
+
+  res.render('upload');
+});
+
+
+//ruta para formulaario post
+ruta.post('/upload', async (req, res) => {
+    const image = new Image();
+    image.title = req.body.title;
+    image.description = req.body.description;
+    image.filename = req.file.filename;
+    image.path = '/img/uploads/' + req.file.filename;
+    image.originalname = req.file.originalname;
+    image.mimetype = req.file.mimetype;
+    image.size = req.file.size;
+
+    await image.save();//para guardar la imagen*/
+
+    res.redirect('/');
+});
+//mostrar una imagen
+ruta.get('/image/:id', (req, res)=>{
+
+    res.send('Perfil de imagen')
+});
+//ruta para eliminar imagen
+
+ruta.get('/image/:id/delete',(req, res)=>{
+    res.send('eliminado')
+});
+
+
+
+
+
+module.exports = ruta;
